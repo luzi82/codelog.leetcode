1083b0e360f79ce343965f63a003bddf3f627975
Luzi82 <luzi82@gmail.com>
2017-09-28 00:25:17 +0800

    slower but easiler to read

--- 000218/code.py
+++ 000218/code.py
@@ -7,60 +7,77 @@ class Solution(object):
         :type buildings: List[List[int]]
         :rtype: List[List[int]]
         """
+        # special handle edge case
         if len(buildings) == 0:
             return []
         
-        # convert buildlings(uvh) to shuv list
-        shuv_list = [ (-b[2], b[2], b[0], b[1]) for b in buildings ]
+        # u: start of building
+        # v: end of building
+        # h: height of building
+        # s: -h, sort key in heap, higher h pop earlier
+        # uvh: [u,v,h], buildings input format
+        # suvh: [s,u,v,h], calculation format
+
+        # convert buildlings(uvh) to suvh list
+        suvh_list = [ ((-uvh[2],)+ tuple(uvh)) for uvh in buildings ]
         
-        # append (uv=max_v+1,h=-1) at end, for easy cal at last
-        max_v = max([shuv[3] for shuv in shuv_list])
-        shuv_list += [(1,-1,max_v+1,max_v+1)]
+        # append (uv=max_v+1,h=0) at end, for easy calc at last
+        max_v = max([suvh[2] for suvh in suvh_list])
+        suvh_list += [(0,max_v+1,max_v+1,0)]
         
+        # output stuff
         result_queue = deque()
         
-        shuv_heap = []
-        done_x = None
-        last_result_h = 0
+        # loop stuff
         
-        for shuv in shuv_list:
-            #print(done_x,shuv,sorted(shuv_heap))
-            _,h,u,v = shuv
+        done_x = None # cursor
+        suvh_heap = [] # store suvh, which u <= done_x
 
-            if len(shuv_heap) == 0:
+        for suvh in suvh_list:
+            #print(done_x,suvh,sorted(suvh_heap))
+            _,u,v,h = suvh
+
+            if len(suvh_heap) == 0:
                 done_x = u
-                heapq.heappush(shuv_heap,shuv)
+                heapq.heappush(suvh_heap,suvh)
                 continue
             
             # u >= done_x
             
             if u == done_x:
-                heapq.heappush(shuv_heap,shuv)
+                heapq.heappush(suvh_heap,suvh)
                 continue
             
             # u > done_x
             
-            while(True):
-                if len(shuv_heap) == 0:
-                    if last_result_h != 0:
-                        result_queue.append([done_x, 0])
-                        last_result_h = 0
+            # find all skyline in [done_x,u), burn suvh_heap
+            while(done_x < u):
+                if len(suvh_heap) == 0:
+                    # no building in [done_x,u)
+                    result_queue.append([done_x,0])
+                    done_x = u
                     break
                 
-                top_shuv = shuv_heap[0]
-                #print(top_shuv)
-                if top_shuv[3] <= done_x:
-                    heapq.heappop(shuv_heap)
+                _,top_u,top_v,top_h = suvh_heap[0]
+
+                # skip passed building
+                if top_v <= done_x:
+                    heapq.heappop(suvh_heap)
                     continue
-                if top_shuv[1] != last_result_h:
-                    #print('output {}'.format(top_shuv))
-                    result_queue.append([done_x, top_shuv[1]])
-                    last_result_h = top_shuv[1] 
-                done_x = min(u, top_shuv[3])
-                if done_x == u:
-                    break
+
+                result_queue.append([done_x, top_h])
+                done_x = min(u, top_v)
 
             done_x = u
-            heapq.heappush(shuv_heap,shuv)
+            heapq.heappush(suvh_heap,suvh)
 
-        return list(result_queue)
+        # remove all point which h same as last
+        result_list = []
+        last_h = 0
+        for xh in result_queue:
+            if xh[1] == last_h:
+                continue
+            result_list.append(xh)
+            last_h = xh[1]
+
+        return result_list
