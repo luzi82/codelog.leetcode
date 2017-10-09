bdded599b7c8d1d3e209d13629963b3b0ee5a953
Luzi82 <luzi82@gmail.com>
2017-10-09 21:12:23 +0800

    000212

--- 0002xx/000212/Solution.java
+++ 0002xx/000212/Solution.java
@@ -7,10 +7,15 @@ class Solution {
     public static class Tries{
         public String endString = null;
         public Map<Character,Tries> childTriesMap=new HashMap<>();
+        public boolean isEmpty(){
+            if(endString!=null)return false;
+            if(!childTriesMap.isEmpty())return false;
+            return true;
+        }
     }
 
     // assumptions:
-    // - words does not contains emptry string
+    // - words does not contains empty string
     // - no repeat words
     // - word not too long, break recursion
     public List<String> findWords(char[][] board, String[] words) {
@@ -31,20 +36,54 @@ class Solution {
         Tries rootTries = new Tries();
         for(char[] wordCharAry:wordCharAryAry){
             Tries tries = rootTries;
-            while(char k:wordCharAry){
+            for(char k:wordCharAry){
                 if(!tries.childTriesMap.containsKey(k)){
                     tries.childTriesMap.put(k,new Tries());
                 }
                 tries = tries.childTriesMap.get(k);
             }
-            tries.endString = newString(wordCharAry);
+            tries.endString = new String(wordCharAry);
         }
         
-        // search
+        // search by dfs
+        int height = board.length;
+        int width = board[0].length;
+        boolean[][] doneAryAry=new boolean[height][width];
+        for(int i=0;i<height;++i)for(int j=0;j<width;++j){
+            doneAryAry[i][j]=false;
+        }
+        
+        for(int i=0;i<height;++i)for(int j=0;j<width;++j){
+            searchDfs(board,rootTries,height,width,i,j,doneAryAry,retList);
+        }
         
         // return
-        
-        
+        return retList;
+    }
+
+    public static void searchDfs(char[][] board,Tries tries,int h,int w,int i,int j,boolean[][] doneAryAry,LinkedList<String> retList){
+        if(i<0)return;
+        if(j<0)return;
+        if(i>=h)return;
+        if(j>=w)return;
+        if(doneAryAry[i][j])return;
+        char c=board[i][j];
+        if(!tries.childTriesMap.containsKey(c))return;
+
+        Tries nextTries=tries.childTriesMap.get(c);
+        if(nextTries.endString!=null){
+            retList.push(nextTries.endString);
+            nextTries.endString=null;
+        }
+        doneAryAry[i][j] = true;
+        searchDfs(board,nextTries,h,w,i+1,j,doneAryAry,retList);
+        searchDfs(board,nextTries,h,w,i-1,j,doneAryAry,retList);
+        searchDfs(board,nextTries,h,w,i,j+1,doneAryAry,retList);
+        searchDfs(board,nextTries,h,w,i,j-1,doneAryAry,retList);
+        doneAryAry[i][j] = false;
+        if(nextTries.isEmpty()){
+            tries.childTriesMap.remove(c);
+        }
     }
     
     public static void main(String[] argv){
@@ -52,6 +91,16 @@ class Solution {
             new String[]{"oaan","etae","ihkr","iflv"},
             new String[]{"oath","pea","eat","rain"},
             new String[]{"eat","oath"}
+        );
+        test(
+            new String[]{"oaan","etae","ihkr","iflv"},
+            new String[]{"oath","pea","eat","rain","ea"},
+            new String[]{"eat","oath","ea"}
+        );
+        test(
+            new String[]{"oaan","etae","ihkr","iflv","oaan","etae","ihkr","iflv"},
+            new String[]{"oath","pea","eat","rain","ea"},
+            new String[]{"eat","oath","ea"}
         );
     }
     
