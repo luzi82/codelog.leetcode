0fa7807a589ea20c1ae6b8dae45e258159e99c2d
Luzi82 <luzi82@gmail.com>
2017-10-15 12:02:22 +0800

    modify 000000

--- 000000/Solution.java
+++ 000000/Solution.java
@@ -8,19 +8,4 @@ class Solution {
         return false;
     }
     
-    public static void main(String[] argv){
-        test(123,true);
-    }
-    
-    public static void test(int x,boolean expected){
-        System.out.println(String.format("x=%d, expected=%s",x,expected));
-        Solution solution = new Solution();
-        boolean result = solution.func(x);
-        System.out.println(String.format("result=%s",result));
-        aassert(result == expected);
-    }
-    
-    public static void aassert(boolean cond){
-        if(!cond)throw new AssertionError();
-    }
 }
---
+++ 000000/Test.java
@@ -0,0 +1,22 @@
+import java.lang.AssertionError;
+import java.util.*;
+import java.util.regex.*;
+
+class Test {
+
+    public static void main(String[] argv){
+        test(123,true);
+    }
+    
+    public static void test(int x,boolean expected){
+        System.out.println(String.format("x=%d, expected=%s",x,expected));
+        Solution solution = new Solution();
+        boolean result = solution.func(x);
+        System.out.println(String.format("result=%s",result));
+        aassert(result == expected);
+    }
+    
+    public static void aassert(boolean cond){
+        if(!cond)throw new AssertionError();
+    }
+}
