18b99ba13d91c075b9c4acfbc45cbb98cb38dc43
Luzi82 <luzi82@gmail.com>
2019-04-05 20:22:39 +0800

    update 0

--- 000000/Test.java
+++ 000000/Test.java
@@ -16,12 +16,42 @@ class Test {
         aassert(result == expected);
     }
     
+    public static String join(int[][] ary){
+        StringBuffer sb=new StringBuffer();
+        sb.append("[");
+        boolean isFirst=true;
+        for(int[] v:ary){
+            if(!isFirst){sb.append(",");}
+            isFirst=false;
+            sb.append(join(v));
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public static String join(int[] ary){
+        StringBuffer sb=new StringBuffer();
+        sb.append("[");
+        boolean isFirst=true;
+        for(int v:ary){
+            if(!isFirst){sb.append(",");}
+            isFirst=false;
+            sb.append(Integer.toString(v));
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
     public static String join(Object[] ary){
         StringBuffer sb=new StringBuffer();
+        sb.append("[");
+        boolean isFirst=true;
         for(Object v:ary){
+            if(!isFirst){sb.append(",");}
+            isFirst=false;
             sb.append(v.toString());
-            sb.append(",");
         }
+        sb.append("]");
         return sb.toString();
     }
     
