3befcda8a2a651c40d108b84f8b94819d5035e64
Luzi82 <luzi82@gmail.com>
2019-04-29 08:15:09 +0800

    41

--- 0000xx/000041/Solution.java
+++ 0000xx/000041/Solution.java
@@ -4,17 +4,19 @@ import java.util.regex.*;
 
 class Solution {
     public int firstMissingPositive(int[] nums) {
+        // nums = {3,4,-1,1}
+    
         // 1-index to 0-index
         for(int i=0;i<nums.length;++i){
-            nums[i]--;
+            nums[i]--; // nums = {2,3,-2,0}
         }
         
         for(int i=0;i<nums.length;++i){
-            int j=i;
+            int j=i; // j = [0,nums.length)
             int carry = -1;
-            while(true){
-                int oldNum = nums[j];
-                nums[j] = carry;
+            while(true){ // first case
+                int oldNum = nums[j]; // oldNum = 2
+                nums[j] = carry; // nums = {-1,3,-2,0}
 
                 if(oldNum==j){nums[j]=oldNum;break;}
                 if(oldNum<0)break;
