cbac1f99864c400e1c77dbef6dbaa6bc6a896832
Luzi82 <luzi82@gmail.com>
2019-04-29 11:14:01 +0800

    168

---
+++ 0001xx/000168/Solution.java
@@ -0,0 +1,62 @@
+import java.lang.AssertionError;
+import java.util.*;
+import java.util.regex.*;
+
+class Solution {
+    public String convertToTitle(int n) {
+        long nLong = n;
+    
+        // find digiCount
+        int digiCount = 1;
+        long digiMax = 1;
+        while(digiMax<n){
+            digiMax *= 26;
+            ++digiCount;
+        }
+        
+        // create digiValAry, [1,26,26^2...]
+        long[] digiValAry = new long[digiCount];
+        digiValAry[0] = 1;
+        for(int i=1;i<digiCount;++i){
+            digiValAry[i] = digiValAry[i-1] * 26;
+        }
+        
+        // create digiAry, 100 -> [22,3]
+        int remain = n;
+        int[] digiAry = new int[digiCount];
+        for(int i=digiCount-1;i>=0;--i){
+            digiAry[i] = (int)( remain / digiValAry[i] );
+            remain %= digiValAry[i];
+        }
+        
+        // last non-zero
+        int end = 0;
+        for(int i=0;i<digiAry.length;++i){
+            if(digiAry[i]==0)continue;
+            end = i;
+        }
+        
+        // carry
+        for(int i=0;i<end;++i){
+            if(digiAry[i]!=0)continue;
+            digiAry[i] += 26;
+            digiAry[i+1]--;
+        }
+        
+        // reverse
+        int[] reverseDigiAry = new int[digiAry.length];
+        for(int i=0;i<digiAry.length;++i){
+            reverseDigiAry[i] = digiAry[digiAry.length-1-i];
+        }
+        
+        // build string
+        StringBuffer sb = new StringBuffer();
+        for(int i=0;i<reverseDigiAry.length;++i){
+            if(reverseDigiAry[i]==0)continue;
+            sb.append((char)('A'-1+reverseDigiAry[i]));
+        }
+        
+        // output
+        return sb.toString();
+    }
+}
---
+++ 0001xx/000168/Test.java
@@ -0,0 +1,68 @@
+import java.lang.AssertionError;
+import java.util.*;
+import java.util.regex.*;
+
+class Test {
+
+    public static void main(String[] argv){
+        // given
+        test(1,"A");
+        test(28,"AB");
+        test(701,"ZY");
+        test(702,"ZZ");
+        test(703,"AAA");
+        // judge
+        test(1000000001,"CFDGSXM");
+    }
+    
+    public static void test(int n,String expected){
+        System.out.println(String.format("n=%d, expected=%s",n,expected));
+        Solution solution = new Solution();
+        String result = solution.convertToTitle(n);
+        System.out.println(String.format("result=%s",result));
+        aassert(result.equals(expected));
+    }
+    
+    public static String join(int[][] ary){
+        StringBuffer sb=new StringBuffer();
+        sb.append("[");
+        boolean isFirst=true;
+        for(int[] v:ary){
+            if(!isFirst){sb.append(",");}
+            isFirst=false;
+            sb.append(join(v));
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public static String join(int[] ary){
+        StringBuffer sb=new StringBuffer();
+        sb.append("[");
+        boolean isFirst=true;
+        for(int v:ary){
+            if(!isFirst){sb.append(",");}
+            isFirst=false;
+            sb.append(Integer.toString(v));
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public static String join(Object[] ary){
+        StringBuffer sb=new StringBuffer();
+        sb.append("[");
+        boolean isFirst=true;
+        for(Object v:ary){
+            if(!isFirst){sb.append(",");}
+            isFirst=false;
+            sb.append(v.toString());
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+    
+    public static void aassert(boolean cond){
+        if(!cond)throw new AssertionError();
+    }
+}
