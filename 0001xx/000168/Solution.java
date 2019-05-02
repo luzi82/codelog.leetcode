d5acce1ec3543b8bbb8bcacab185c936b9fb8862
Luzi82 <luzi82@gmail.com>
2019-05-02 20:30:51 +0800

    shorten the code

--- 0001xx/000168/Solution.java
+++ 0001xx/000168/Solution.java
@@ -4,32 +4,15 @@ import java.util.regex.*;
 
 class Solution {
     public String convertToTitle(int n) {
-        long nLong = n;
-    
-        // find digiCount
-        int digiCount = 1;
-        long digiMax = 1;
-        while(digiMax<n){
-            digiMax *= 26;
-            ++digiCount;
-        }
-        
-        // create digiValAry, [1,26,26^2...]
-        long[] digiValAry = new long[digiCount];
-        digiValAry[0] = 1;
-        for(int i=1;i<digiCount;++i){
-            digiValAry[i] = digiValAry[i-1] * 26;
-        }
-        
-        // create digiAry, 100 -> [22,3]
+        // create digiAry, 100 -> [22,3,0...]
         int remain = n;
-        int[] digiAry = new int[digiCount];
-        for(int i=digiCount-1;i>=0;--i){
-            digiAry[i] = (int)( remain / digiValAry[i] );
-            remain %= digiValAry[i];
+        int[] digiAry = new int[20];
+        for(int i=0;i<digiAry.length;++i){
+            digiAry[i] = remain%26;
+            remain /= 26;
         }
-        
-        // last non-zero
+
+        // check last non-zero
         int end = 0;
         for(int i=0;i<digiAry.length;++i){
             if(digiAry[i]==0)continue;
@@ -43,17 +26,11 @@ class Solution {
             digiAry[i+1]--;
         }
         
-        // reverse
-        int[] reverseDigiAry = new int[digiAry.length];
-        for(int i=0;i<digiAry.length;++i){
-            reverseDigiAry[i] = digiAry[digiAry.length-1-i];
-        }
-        
         // build string
         StringBuffer sb = new StringBuffer();
-        for(int i=0;i<reverseDigiAry.length;++i){
-            if(reverseDigiAry[i]==0)continue;
-            sb.append((char)('A'-1+reverseDigiAry[i]));
+        for(int i=digiAry.length-1;i>=0;--i){
+            if(digiAry[i]==0)continue;
+            sb.append((char)('A'-1+digiAry[i]));
         }
         
         // output
