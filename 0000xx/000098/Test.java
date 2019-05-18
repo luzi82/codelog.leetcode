65323cea399a35e61578f82f8a393fda7a4804e1
Luzi82 <luzi82@gmail.com>
2019-05-18 18:09:13 +0800

    fix 98 tree build algo

--- 0000xx/000098/Test.java
+++ 0000xx/000098/Test.java
@@ -19,17 +19,65 @@ class Test {
         aassert(result == expected);
     }
     
-    public static TreeNode toTreeNode(Integer[] x){
-        TreeNode[] tnAry = new TreeNode[x.length];
-        for(int i=0;i<x.length;++i){
-            if(x[i]==null)continue;
-            tnAry[i] = new TreeNode(x[i]);
-            if(i==0)continue;
-            //System.err.println(String.format("%d %d",i,(i-1)/2));
-            if((i-1)%2==0)tnAry[(i-1)/2].left=tnAry[i];
-            else tnAry[(i-1)/2].right=tnAry[i];
+    public static TreeNode toTreeNode(Integer[] vAry){
+        if(vAry.length==0)return null;
+        
+        TreeNode ret = new TreeNode(vAry[0]);
+        
+        LinkedList<TreeNode> bfsQueue = new LinkedList<>();
+        bfsQueue.addLast(ret);
+        
+        int readIdx = 1;
+        while(true){
+            if(readIdx >= vAry.length)break;
+            TreeNode node = bfsQueue.pollFirst();
+            if(vAry[readIdx]!=null){
+                node.left = new TreeNode(vAry[readIdx]);
+                bfsQueue.addLast(node.left);
+            }
+            ++readIdx;
+            if(readIdx >= vAry.length)break;
+            if(vAry[readIdx]!=null){
+                node.right = new TreeNode(vAry[readIdx]);
+                bfsQueue.addLast(node.right);
+            }
+            ++readIdx;
         }
-        return tnAry[0];
+        
+        return ret;
+    }
+    
+    public static Integer[] toIntArray(TreeNode treeNode){
+        if(treeNode==null){return new Integer[0];}
+        LinkedList<Integer> retList = new LinkedList<>();
+        LinkedList<TreeNode> bfsQueue = new LinkedList<>();
+        
+        retList.addLast(treeNode.val);
+        bfsQueue.addLast(treeNode);
+        
+        while(!bfsQueue.isEmpty()){
+            TreeNode node = bfsQueue.pollFirst();
+            if(node.left!=null){
+                retList.addLast(node.left.val);
+                bfsQueue.addLast(node.left);
+            }else{
+                retList.addLast(null);
+            }
+            if(node.right!=null){
+                retList.addLast(node.right.val);
+                bfsQueue.addLast(node.right);
+            }else{
+                retList.addLast(null);
+            }
+        }
+        
+        while(true){
+            if(retList.isEmpty())break;
+            if(retList.getLast()!=null)break;
+            retList.pollLast();
+        }
+        
+        return retList.toArray(new Integer[0]);
     }
     
     public static String join(int[][] ary){
