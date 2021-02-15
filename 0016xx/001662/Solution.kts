b7df3e6d4d3c4c0da47f2845bc8f5c8dafd5823a
luzi82 <luzi82@gmail.com>
2021-02-15 15:00:36 +0000

    1662 kotlin

---
+++ 0016xx/001662/Solution.kts
@@ -0,0 +1,20 @@
+import kotlin.text.*
+
+class Solution {
+    fun arrayStringsAreEqual(word1: Array<String>, word2: Array<String>): Boolean {
+        val sb1 = StringBuilder()
+        for(word in word1){
+          sb1.append(word)
+        }
+
+        val sb2 = StringBuilder()
+        for(word in word2){
+          sb2.append(word)
+        }
+
+        val str1 = sb1.toString();
+        val str2 = sb2.toString();
+
+        return str1.equals(str2);
+    }
+}
