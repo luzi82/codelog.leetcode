861bf9abc28fe8eda715cdd20f3fc721b1fd968a
luzi82 <luzi82@gmail.com>
2021-02-16 15:02:06 +0000

    better 1672

--- 0014xx/001470/Solution.kts
+++ 0014xx/001470/Solution.kts
@@ -1,5 +1,5 @@
 class Solution {
     fun shuffle(nums: IntArray, n: Int): IntArray {
-        return (0..(2*n-1)).map{(it/2,it%2==0)}.map{nums[it[0]+it[1]*n]}.toIntArray()
+        return (0..(2*n-1)).map{nums[it/2+(it%2)*n]}.toIntArray()
     }
 }
