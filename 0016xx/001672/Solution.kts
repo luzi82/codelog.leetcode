9dab540eea839a3c6c83db9975da9b64d93c8d86
luzi82 <luzi82@gmail.com>
2021-02-16 15:17:39 +0000

    better 1672

--- 0016xx/001672/Solution.kts
+++ 0016xx/001672/Solution.kts
@@ -1,5 +1,6 @@
 class Solution {
     fun maximumWealth(accounts: Array<IntArray>): Int {
-        return accounts.map{it.sum()}.max()?:0;
+        // return accounts.map{it.sum()}.max()?:0;
+        return accounts.asSequence.map{it.sum()}.maxOrNull()?:0;
     }
 }
