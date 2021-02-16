73340086d9a44f7e9ad64c15b969936e8e16b312
luzi82 <luzi82@gmail.com>
2021-02-16 14:45:44 +0000

    better solution

--- 0014xx/001431/Solution.kts
+++ 0014xx/001431/Solution.kts
@@ -1,6 +1,7 @@
 class Solution {
     fun kidsWithCandies(candies: IntArray, extraCandies: Int): BooleanArray {
         val maxCandy = candies.max() ?: 0
-        return candies.map{it+extraCandies}.map{it>=maxCandy}.toBooleanArray()
+        val passCandy = maxCandy - extraCandies
+        return candies.map{it>=passCandy}.toBooleanArray()
     }
 }
