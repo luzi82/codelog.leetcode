57c01002b3f9ccf902c41bbf0eeff1a98e0777b7
luzi82 <luzi82@gmail.com>
2021-04-15 01:33:10 +0000

    add init problem script

---
+++ init-problem.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+if [ "$#" -ne 1 ]; then
+  echo "${0} [PROBLEM_NUM]"
+  exit 1
+fi
+
+Z00=000000${1}
+#echo ${Z00}
+Z0=${Z00:(-6)}
+#echo ${Z0}
+Z0X=${Z0:0:4}xx
+#echo ${Z0X}
+
+mkdir -p ${Z0X}
+cp -R 000000 ${Z0X}/${Z0}
