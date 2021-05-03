fd638228098b4fff431d9fc8fe6edf6ee86576bc
luzi82 <luzi82@gmail.com>
2021-05-03 04:40:19 +0000

    1131 O(n)

--- 0011xx/001131/java/Solution.java
+++ 0011xx/001131/java/Solution.java
@@ -4,17 +4,24 @@ import java.util.regex.*;
 
 class Solution {
     public int maxAbsValExpr(int[] arr1, int[] arr2) {
-        int ans = 0;
-        for(int j=0;j<arr2.length;++j){
-            for(int i=0;i<j;++i){
-                int v = 0;
-                v += Math.abs(arr1[i]-arr1[j]);
-                v += Math.abs(arr2[i]-arr2[j]);
-                v += Math.abs(i-j);
-                if(v<ans)continue;
-                ans=v;
-            }
+        int ret = 0;
+
+        int l0 =  arr1[0]+arr2[0];
+        int l1 =  arr1[0]-arr2[0];
+        int l2 = -arr1[0]+arr2[0];
+        int l3 = -arr1[0]-arr2[0];
+
+        for(int i=1;i<arr1.length;++i){
+          ret = Math.max(ret,l0-arr1[i]-arr2[i]+i);
+          ret = Math.max(ret,l1-arr1[i]+arr2[i]+i);
+          ret = Math.max(ret,l2+arr1[i]-arr2[i]+i);
+          ret = Math.max(ret,l3+arr1[i]+arr2[i]+i);
+          l0 = Math.max(l0, arr1[i]+arr2[i]-i);
+          l1 = Math.max(l1, arr1[i]-arr2[i]-i);
+          l2 = Math.max(l2,-arr1[i]+arr2[i]-i);
+          l3 = Math.max(l3,-arr1[i]-arr2[i]-i);
         }
-        return ans;
+
+        return ret;
     }
 }
