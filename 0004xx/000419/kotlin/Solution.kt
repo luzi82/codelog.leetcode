5918614629c0faa2ece9eca1de7b860da7f0c283
luzi82 <luzi82@gmail.com>
2021-05-07 02:38:09 +0000

    419

---
+++ 0004xx/000419/kotlin/Solution.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun countBattleships(board: Array<CharArray>): Int {
+        val ii=board.size
+        val jj=board[0].size
+
+        var ret=0
+        for(i in 0..(ii-1)){
+          for(j in 0..(jj-1)){
+            if(board[i][j]!='X'){continue}
+            
+          }
+        }
+
+        return ret
+    }
+}
