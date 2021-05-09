2c4de38d9f37ae0ef14d84a2cd15ac8a7d281a8e
luzi82 <luzi82@gmail.com>
2021-05-09 06:53:38 +0000

    1857

--- 0018xx/001857/kotlin/Test.kt
+++ 0018xx/001857/kotlin/Test.kt
@@ -3,6 +3,8 @@ import java.util.*
 fun main() {
   test("abaca","[[0,1],[0,2],[2,3],[3,4]]",3)
   test("a","[[0,0]]",-1)
+  test("ab","[[0,1],[1,1]]",-1)
+  test("aaa","[[1,2],[2,1]]",-1)
 }
 
 fun test(colors:String, edgess:String, expected: Int) {
