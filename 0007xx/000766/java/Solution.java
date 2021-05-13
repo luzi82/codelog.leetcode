27ccdbcb347695069a6dfc6a510547362f3b1f41
luzi82 <luzi82@gmail.com>
2021-05-13 14:21:19 +0800

    766

---
+++ 0007xx/000766/java/Solution.java
@@ -0,0 +1,14 @@
+import java.lang.AssertionError;
+import java.util.*;
+import java.util.regex.*;
+
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for(int i=1;i<matrix.length;++i){
+            for(int j=1;j<matrix[0].length;++j){
+                if(matrix[i][j]!=matrix[i-1][j-1]){return false;}
+            }
+        }
+        return true;
+    }
+}
