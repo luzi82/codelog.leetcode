6dfce642737821dc3e6fe397a58cb82a03ba5179
luzi82 <luzi82@gmail.com>
2021-05-17 17:14:40 +0800

    improve 980

--- 0009xx/000980/java/Solution.0.java
+++ 0009xx/000980/java/Solution.0.java
@@ -7,29 +7,22 @@ class Solution {
     public int uniquePathsIII(int[][] grid) {
         iCnt = grid.length;
         jCnt = grid[0].length;
-        kCnt = iCnt*jCnt;
         
-        BitSet startBitSet = new BitSet(kCnt);
         int startI=-1;
         int startJ=-1;
         int zeroCnt = 0;
         for(int i=0;i<iCnt;++i){
             for(int j=0;j<jCnt;++j){
-                int k = ijToK(i,j);
                 if(grid[i][j]==1){
                     startI=i;startJ=j;
                 }
                 if(grid[i][j]==0){
                     ++zeroCnt;
-                    startBitSet.set(k,true);
-                }
-                if(grid[i][j]==2){
-                    endI=i;endJ=j;
                 }
             }
         }
         
-        State startState = new State(startBitSet,startI,startJ,zeroCnt);
+        State startState = new State(grid,startI,startJ,zeroCnt);
         stateToPathCntMap.put(startState,1);
         stateQueue.addLast(startState);
         
@@ -54,16 +47,16 @@ class Solution {
         if(ii>=iCnt)return;
         if(jj<0)return;
         if(jj>=jCnt)return;
-        int kk = ijToK(ii,jj);
         int pathCnt = stateToPathCntMap.get(state);
-        if((ii==endI)&&(jj==endJ)){
+        if(state.grid[ii][jj]==2){
             if(state.zeroCnt<=0){
                 ret += pathCnt;
             }
-        }else if(state.bs.get(kk)){
-            BitSet newBs = (BitSet)state.bs.clone();
-            newBs.set(kk,false);
-            State newState = new State(newBs,ii,jj,state.zeroCnt-1);
+        }else if(state.grid[ii][jj]==0){
+            int[][] newGrid = clone(state.grid);
+            newGrid[state.i][state.j]=-1;
+            newGrid[ii][jj]=1;
+            State newState = new State(newGrid,ii,jj,state.zeroCnt-1);
             if(!stateToPathCntMap.containsKey(newState)){
                 stateQueue.addLast(newState);
                 stateToPathCntMap.put(newState,pathCnt);
@@ -73,25 +66,25 @@ class Solution {
         }
     }
     
-    int iCnt;int jCnt;int kCnt;
-    int endI;int endJ;
+    int iCnt;int jCnt;
     
     class State{
-        BitSet bs;
-        int i;int j;
-        int zeroCnt;
-        public State(BitSet bs,int i,int j,int zeroCnt){
-            this.bs = bs;
+        int[][] grid;
+        int i;int j;int zeroCnt;
+        public State(int[][]grid,int i,int j,int zeroCnt){
+            this.grid = grid;
             this.i=i;
             this.j=j;
             this.zeroCnt=zeroCnt;
         }
         public int hashCode(){
-            return Objects.hash(bs,i,j,zeroCnt);
+            return Objects.hash(grid,i,j,zeroCnt);
         }
         public boolean equals(Object otherr){
             State other = (State)otherr;
-            if(!this.bs.equals(other.bs))return false;
+            if(!Arrays.deepEquals(this.grid,other.grid)){
+                return false;
+            }
             if(this.i!=other.i)return false;
             if(this.j!=other.j)return false;
             if(this.zeroCnt!=other.zeroCnt)return false;
@@ -99,8 +92,12 @@ class Solution {
         }
     }
     
-    int ijToK(int i,int j){
-      return i*jCnt+j;
+    int[][] clone(int[][] oriAryAry){
+        int[][] retAry = new int[iCnt][jCnt];
+        for(int i=0;i<iCnt;++i)for(int j=0;j<jCnt;++j){
+            retAry[i][j] = oriAryAry[i][j];
+        }
+        return retAry;
     }
     
 }
