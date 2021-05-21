7c6017f8b3bfc75fc3e3d2d5d78e5bfe4a0767e4
luzi82 <luzi82@gmail.com>
2021-05-21 12:10:31 +0800

    1696

---
+++ 0016xx/001696/java/Solution.java
@@ -0,0 +1,37 @@
+import java.lang.AssertionError;
+import java.util.*;
+import java.util.regex.*;
+
+class Solution {
+    public int maxResult(int[] nums, int k) {
+        int[] idxToSum = new int[nums.length];
+        for(int idx=0;idx<idxToSum.length;++idx){
+          idxToSum[idx]=Integer.MIN_VALUE;
+        }
+        idxToSum[0] = nums[0];
+        int idx=0;
+        while(true){
+          if(idx>=idxToSum.length-1)break;
+          int oldSum = idxToSum[idx];
+          int bestIdx = -1;
+          int bestV = Integer.MIN_VALUE;
+          for(int j=1;j<=k;++j){
+            int idx0 = idx+j;
+            if(idx0>=nums.length)break;
+            int v = nums[idx0];
+            int newSum = oldSum+v;
+            idxToSum[idx0] = Math.max(idxToSum[idx0],newSum);
+            if(idxToSum[idx0]>=bestV){
+              bestV=idxToSum[idx0];
+              bestIdx=idx0;
+            }
+            if(v>=0){
+              bestIdx=idx0;
+              break;
+            }
+          }
+          idx=bestIdx;
+        }
+        return idxToSum[idxToSum.length-1];
+    }
+}
